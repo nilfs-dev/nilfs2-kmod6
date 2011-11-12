d6f8ee9ad3962486014f57a58345f035027fd9f5
Ryusuke Konishi <konishi.ryusuke@lab.ntt.co.jp>
2011-11-13 01:51:34 +0900

    nilfs2-kmod: describe unapplied patches in kern_feature.h
    This records description of unapplied patches during backporting newer
    patches.
    
    Signed-off-by: Ryusuke Konishi <konishi.ryusuke@lab.ntt.co.jp>

--- fs/nilfs2/kern_feature.h
+++ fs/nilfs2/kern_feature.h
@@ -54,5 +54,38 @@ inode_init_owner(struct inode *inode, const struct inode *dir, mode_t mode)
 }
 #endif
 
+/*
+ * The following patches are left unapplied during backporting later
+ * patches:
+ *
+ * commit 6a47dc1418682c83d603b491df1d048f73aa973e
+ *   "nilfs: fix breakage caused by barrier flag changes"
+ * commit 7ea8085910ef3dd4f3cad6845aaa2b580d39b115
+ *   "drop unused dentry argument to ->fsync"
+ * commit 7b6d91daee5cac6402186ff224c3af39d79f4a0e
+ *   "block: unify flags for struct bio and struct request"
+ * commit eafdc7d190a944c755a9fe68573c193e6e0217e7
+ *   "sort out blockdev_direct_IO variants"
+ * commit f4e420dc423148fba637af1ab618fa8896dfb2d6
+ *   "clean up write_begin usage for directories in pagecache"
+ * commit 6e1db88d536adcbbfe562b2d4b7d6425784fff12
+ *   "introduce __block_write_begin"
+ * commit 155130a4f7848b1aac439cab6bda1a175507c71c
+ *   "get rid of block_write_begin_newtrunc"
+ * commit 1025774ce411f2bd4b059ad7b53f0003569b74fa
+ *   "remove inode_setattr"
+ * commit a4ffdde6e56fdf8c34ddadc2674d6eb978083369
+ *   "simplify checks for I_CLEAR/I_FREEING"
+ * commit 6fd1e5c994c392ebdbe45600051b2a32ec4860f1
+ *   "convert nilfs2 to ->evict_inode()"
+ * commit 87e99511ea54510ffb60b98001d108794d5037f8
+ *   "kill BH_Ordered flag"
+ * commit 1cb0c924fa2d616e5e3b5bc62d97191aac9ff442
+ *   "nilfs2: wait for discard to finish"
+ * commit f8c131f5b6ffc899a70b30e541f367d47f89691c
+ *   "nilfs2: replace barriers with explicit flush / FUA usage"
+ * commit dd3932eddf428571762596e17b65f5dc92ca361b
+ *   "block: remove BLKDEV_IFL_WAIT"
+ */
 
 #endif /* NILFS_KERN_FEATURE_H */
